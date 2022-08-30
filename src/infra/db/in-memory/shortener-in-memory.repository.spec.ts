ba9c4d7eb4bdb9767b53eac8a3c6294b54adeff7
Juliano Costa Silva <apfjuliano@gmail.com>
2022-08-30 11:35:19 -0300

    refactor: reusing lists variables on all tests

--- src/infra/db/in-memory/shortener-in-memory.repository.spec.ts
+++ src/infra/db/in-memory/shortener-in-memory.repository.spec.ts
@@ -1,64 +1,52 @@
-import { ShortenerInMemoryRepository } from "./shortener-in-memory.repository";
-import { Shortener } from "../../../domain/shortener.entity";
+import { faker } from '@faker-js/faker';
+import { ShortenerInMemoryRepository } from './shortener-in-memory.repository';
+import { Shortener } from '../../../domain/shortener.entity';
 import { format } from 'date-fns';
 import crypto from 'crypto';
 
-let url: string = 'https://www.google.com';
+let url = faker.internet.url();
+const urlList: string[] = [];
+const shorteners: Shortener[] = [];
+const repository = new ShortenerInMemoryRepository();
+
+beforeAll(async () => {
+  for (let i = 0; i < 3; i++) {
+    urlList.push(faker.internet.url());
+    const shortener = Shortener.create(urlList[i]);
+    await repository.insert(shortener);
+    shorteners.push(shortener);
+  }
+});
 
 describe('ShortenerInMemory Repository Tests', () => {
   it('should insert a new shortener', async () => {
     let shortener = Shortener.create(url);
     const repository = new ShortenerInMemoryRepository();
     await repository.insert(shortener);
-    expect(repository.items).toHaveLength(1);
-    expect(repository.items[0]).toStrictEqual(shortener);
+    expect(repository.items[repository.items.length - 1]).toStrictEqual(
+      shortener,
+    );
   });
   it('should find URL by shortURL', async () => {
-    let shortener = Shortener.create(url);
-    const repository = new ShortenerInMemoryRepository();
-    await repository.insert(shortener);
-    expect(repository.items).toHaveLength(1);
-    const theURL = await repository.findURL(shortener.shortURL);
+    const theURL = await repository.findURL(shorteners[0].shortURL);
     expect(repository.items[0].url).toBe(theURL);
   });
   it('should not find URL by shortURL', async () => {
     let shortURL = `${process.env.URL}${Math.random()
       .toString(36)
       .substring(5)}`;
-    let shortener = Shortener.create(
-      `${url}/${Math.random().toString(36).substring(5)}`,
-    );
-    const repository = new ShortenerInMemoryRepository();
-    await repository.insert(shortener);
-    expect(repository.items).toHaveLength(1);
     const theURL = await repository.findURL(shortURL);
     expect(theURL).toBeNull();
   });
   it('should find ShortURL by findShortURLById', async () => {
-    let shortener = Shortener.create(url);
-    const repository = new ShortenerInMemoryRepository();
-    await repository.insert(shortener);
-    expect(repository.items).toHaveLength(1);
-    const shortURL = await repository.findShortURLById(shortener.id);
+    const shortURL = await repository.findShortURLById(shorteners[0].id);
     expect(repository.items[0].shortURL).toBe(shortURL);
   });
   it('should not find ShortURL by findShortURLById', async () => {
-    let shortener = Shortener.create(url);
-    const repository = new ShortenerInMemoryRepository();
-    await repository.insert(shortener);
-    expect(repository.items).toHaveLength(1);
     const shortURL = await repository.findShortURLById(crypto.randomUUID());
     expect(shortURL).toBeNull();
   });
   it('should findAllByDate shorteners', async () => {
-    const shorteners: Shortener[] = [];
-    const repository = new ShortenerInMemoryRepository();
-    for (let i = 0; i < 3; i++) {
-      let shortener = Shortener.create(`https://www.google.com/rota${i + 1}`);
-      await repository.insert(shortener);
-      shorteners.push(shortener);
-    }
-    // const date = format(new Date(), 'dd/MM/yyyy')
     const shortenerList = await repository.findAllByDate(new Date());
 
     expect(repository.items).toHaveLength(3);
@@ -75,13 +63,6 @@ describe('ShortenerInMemory Repository Tests', () => {
     );
   });
   it('should findAll shorteners', async () => {
-    const shorteners: Shortener[] = [];
-    const repository = new ShortenerInMemoryRepository();
-    for (let i = 0; i < 3; i++) {
-      let shortener = Shortener.create(`https://www.google.com/rota${i + 1}`);
-      await repository.insert(shortener);
-      shorteners.push(shortener);
-    }
     const shortenerList = await repository.findAll();
 
     expect(repository.items).toHaveLength(3);
@@ -89,6 +70,6 @@ describe('ShortenerInMemory Repository Tests', () => {
     expect(repository.items).toHaveLength(shortenerList.length);
     expect(repository.items[0]).toStrictEqual(shorteners[0]);
     expect(repository.items[1]).toStrictEqual(shortenerList[1]);
-    expect(repository.items[2].url).toBe('https://www.google.com/rota3');
+    expect(repository.items[2].url).toBe(urlList[2]);
   });
 });
--- src/infra/db/typeorm/shortener-typeorm.repository.spec.ts
+++ src/infra/db/typeorm/shortener-typeorm.repository.spec.ts
@@ -1,77 +1,55 @@
+import { faker } from '@faker-js/faker';
 // import { dataSourceInMemory as dataSource } from './dataSource';
 import { dataSourceInMemory as dataSource } from './dataSource';
 import { Shortener } from '../../../domain/shortener.entity';
 import { ShortenerTypeOrmRepository } from './shortener-typeorm.repository';
 import crypto from 'crypto';
 
-const url: string = 'https://www.google.com';
+const urlList: string[] = [];
+const shorteners: Shortener[] = [];
+const ormRepo = dataSource.getRepository(Shortener);
+const repository = new ShortenerTypeOrmRepository(ormRepo);
 
-beforeEach(async () => {
+beforeAll(async () => {
   await dataSource.initialize();
+  for (let i = 0; i < 3; i++) {
+    urlList.push(faker.internet.url());
+    const shortener = Shortener.create(urlList[i]);
+    await repository.insert(shortener);
+    shorteners.push(shortener);
+  }
 });
-afterEach(async () => {
+afterAll(async () => {
   await dataSource.destroy();
 });
 
 describe('ShortenerTypeOrmRepository Tests', () => {
   it('should insert a new shortener', async () => {
-    // await dataSource.initialize();
-    let shortener = Shortener.create(url);
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    await repository.insert(shortener);
+    const shortenerFound = await ormRepo.findOneBy({ id: shorteners[0].id });
 
-    const shortenerFound = await ormRepo.findOneBy({ id: shortener.id });
-
-    expect(shortenerFound?.toJSON()).toStrictEqual(shortener.toJSON());
-    expect(shortenerFound).toStrictEqual(shortener);
+    expect(shortenerFound?.toJSON()).toStrictEqual(shorteners[0].toJSON());
+    expect(shortenerFound).toStrictEqual(shorteners[0]);
   });
   it('should find URL by shortURL', async () => {
-    let shortener = Shortener.create(url);
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    await repository.insert(shortener);
-    const theURL = await repository.findURL(shortener.shortURL);
-    expect(theURL).toBe(url);
+    const theURL = await repository.findURL(shorteners[1].shortURL);
+    expect(theURL).toBe(urlList[1]);
   });
   it('should not find URL by shortURL', async () => {
     let shortURL = `${process.env.URL}${Math.random()
       .toString(36)
       .substring(5)}`;
-    let shortener = Shortener.create(
-      `${url}/${Math.random().toString(36).substring(5)}`,
-    );
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    await repository.insert(shortener);
     const theURL = await repository.findURL(shortURL);
     expect(theURL).toBeNull();
   });
   it('should find ShortURL by findShortURLById', async () => {
-    let shortener = Shortener.create(url);
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    await repository.insert(shortener);
-    const shortURL = await repository.findShortURLById(shortener.id);
-    expect(shortener.shortURL).toBe(shortURL);
+    const shortURL = await repository.findShortURLById(shorteners[0].id);
+    expect(shorteners[0].shortURL).toBe(shortURL);
   });
   it('should not find ShortURL by findShortURLById', async () => {
-    let shortener = Shortener.create(url);
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    await repository.insert(shortener);
     const shortURL = await repository.findShortURLById(crypto.randomUUID());
     expect(shortURL).toBeNull();
   });
   it('should findAllByDate shorteners', async () => {
-    const shorteners: Shortener[] = [];
-    const ormRepo = dataSource.getRepository(Shortener);
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-    for (let i = 0; i < 3; i++) {
-      let shortener = Shortener.create(`https://www.google.com/rota${i + 1}`);
-      await repository.insert(shortener);
-      shorteners.push(shortener);
-    }
     const date = new Date();
     const shortenerList = await repository.findAllByDate(date);
     expect(shortenerList).toHaveLength(3);
@@ -80,17 +58,9 @@ describe('ShortenerTypeOrmRepository Tests', () => {
     expect(shortenerList[0].createdAt).toStrictEqual(shorteners[0].createdAt);
     expect(shortenerList[1].createdAt).toStrictEqual(shorteners[1].createdAt);
     expect(shortenerList[2].createdAt).toStrictEqual(shorteners[2].createdAt);
+    expect(shortenerList[2].url).toStrictEqual(urlList[2]);
   });
   it('should find all shorteners', async () => {
-    const ormRepo = dataSource.getRepository(Shortener);
-    const shorteners: Shortener[] = [];
-    const repository = new ShortenerTypeOrmRepository(ormRepo);
-
-    for (let i = 0; i < 3; i++) {
-      const shortener = Shortener.create(`https://www.google.com/rota${i + 1}`);
-      await repository.insert(shortener);
-      shorteners.push(shortener);
-    }
     const shortenerList = await repository.findAll();
 
     expect(shortenerList).toHaveLength(3);
@@ -98,6 +68,6 @@ describe('ShortenerTypeOrmRepository Tests', () => {
     expect(shortenerList).toHaveLength(shortenerList.length);
     expect(shortenerList[0]).toStrictEqual(shorteners[0]);
     expect(shortenerList[1]).toStrictEqual(shortenerList[1]);
-    expect(shortenerList[2].url).toBe('https://www.google.com/rota3');
+    expect(shortenerList[2].url).toBe(urlList[2]);
   });
 });
