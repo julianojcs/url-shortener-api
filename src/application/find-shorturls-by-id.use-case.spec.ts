ab666ef183ae81cfb789917f2e9a083a981b8e0d
Juliano Costa Silva <apfjuliano@gmail.com>
2022-08-31 18:10:10 -0300

    test: adding missing test "shouldn't find..."

--- src/application/find-shorturls-by-id.use-case.spec.ts
+++ src/application/find-shorturls-by-id.use-case.spec.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { faker } from '@faker-js/faker';
 import { Shortener } from '../domain/shortener.entity';
 import { dataSourceInMemory as dataSource } from '../infra/db/typeorm/dataSource';
@@ -7,6 +8,7 @@ import { CreateShortenerUseCase } from './create-shortener.use-case';
 import { FindShortURLByIdUseCase } from './find-shorturls-by-id.use-case';
 
 const url = faker.internet.url();
+const id = crypto.randomUUID();
 
 describe('FindShortURLByIdUseCase Tests', () => {
   it('should find a shortURL by id, using in-memory repository', async () => {
@@ -17,6 +19,14 @@ describe('FindShortURLByIdUseCase Tests', () => {
     const shortURLFound = await findShortURLByIdUseCase.execute(shortener.id);
     expect(shortURLFound).toBe(shortener.shortURL);
   });
+  it("shouldn't find a shortURL by id, using in-memory repository", async () => {
+    const repository = new ShortenerInMemoryRepository();
+    const createShortenerUseCase = new CreateShortenerUseCase(repository);
+    await createShortenerUseCase.execute(url);
+    const findShortURLByIdUseCase = new FindShortURLByIdUseCase(repository);
+    const shortURLFound = await findShortURLByIdUseCase.execute(id);
+    expect(shortURLFound).toBeNull();
+  });
   it('should find a shortURL by id, using database repository', async () => {
     await dataSource.initialize();
     const ormRepo = dataSource.getRepository(Shortener);
@@ -26,6 +36,17 @@ describe('FindShortURLByIdUseCase Tests', () => {
     const findShortURLByIdUseCase = new FindShortURLByIdUseCase(repository);
     const shortURLFound = await findShortURLByIdUseCase.execute(shortener.id);
     expect(shortURLFound).toBe(shortener.shortURL);
+    await dataSource.destroy();
+  });
+  it("shouldn't find a shortURL by id, using database repository", async () => {
+    await dataSource.initialize();
+    const ormRepo = dataSource.getRepository(Shortener);
+    const repository = new ShortenerTypeOrmRepository(ormRepo);
+    const createShortenerUseCase = new CreateShortenerUseCase(repository);
+    await createShortenerUseCase.execute(url);
+    const findShortURLByIdUseCase = new FindShortURLByIdUseCase(repository);
+    const shortURLFound = await findShortURLByIdUseCase.execute(id);
+    expect(shortURLFound).toBeNull();
     await dataSource.destroy();
   });
 });
--- src/application/find-url.use-case.spec.ts
+++ src/application/find-url.use-case.spec.ts
@@ -7,6 +7,7 @@ import { CreateShortenerUseCase } from './create-shortener.use-case';
 import { FindURLUseCase } from './find-url.use-case';
 
 const url = faker.internet.url();
+const shortURL = `${process.env.URL}${Math.random().toString(36).substring(5)}`;
 
 describe('FindURLUseCase Tests', () => {
   it('should find a URL by shortURL, using in-memory repository', async () => {
@@ -18,6 +19,16 @@ describe('FindURLUseCase Tests', () => {
     expect(URLFound).toBe(shortener.url);
     expect(URLFound).toBe(url);
   });
+  it("shouldn't find a URL by shortURL, using in-memory repository", async () => {
+    const repository = new ShortenerInMemoryRepository();
+    const createShortenerUseCase = new CreateShortenerUseCase(repository);
+    const shortener = await createShortenerUseCase.execute(url);
+    const findURLUseCase = new FindURLUseCase(repository);
+    const URLFound = await findURLUseCase.execute(shortURL);
+    expect(URLFound).toBeNull();
+    expect(URLFound).not.toBe(shortener.url);
+    expect(URLFound).not.toBe(url);
+  });
   it('should find a URL by shortURL, using database repository', async () => {
     await dataSource.initialize();
     const ormRepo = dataSource.getRepository(Shortener);
@@ -28,6 +39,19 @@ describe('FindURLUseCase Tests', () => {
     const URLFound = await findURLUseCase.execute(shortener.shortURL);
     expect(URLFound).toBe(shortener.url);
     expect(URLFound).toBe(url);
+    await dataSource.destroy();
+  });
+  it("shouldn't find a URL by shortURL, using database repository", async () => {
+    await dataSource.initialize();
+    const ormRepo = dataSource.getRepository(Shortener);
+    const repository = new ShortenerTypeOrmRepository(ormRepo);
+    const createShortenerUseCase = new CreateShortenerUseCase(repository);
+    const shortener = await createShortenerUseCase.execute(url);
+    const findURLUseCase = new FindURLUseCase(repository);
+    const URLFound = await findURLUseCase.execute(shortURL);
+    expect(URLFound).toBeNull();
+    expect(URLFound).not.toBe(shortener.url);
+    expect(URLFound).not.toBe(url);
     await dataSource.destroy();
   });
 });
